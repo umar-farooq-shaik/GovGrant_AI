18dfbcf2cfdb9c03c7a62295ebd572f7ea769bd7
Shaik Umar Farooq <rr200193@rguktrkv.ac.in>
2025-05-30 17:56:38 +0000

    add the following image to homepage[1200*400], image url:Here's the image you requested for the government website, incorporating the diverse group of individuals, positive atmosphere, technology symbolizing accessible information, and the specified colors: http://googleusercontent.com/image_generation_content/0

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
